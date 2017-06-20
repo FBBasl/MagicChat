ed71579cf473937d4415fdbe91b761890a86f9dc
Асылбек Жилкайдаров <sorryjoinserver@gmail.com>
2017-06-20 10:51:35 +0300

    Дорабатывал Вход

---
+++ MagicChat/PagerTabStripViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PagerTabStripViewController.swift
+//  MagicChat
+//
+//  Created by Асылбек Жилкайдаров on 20.06.17.
+//  Copyright © 2017 Test Inc. All rights reserved.
+//
+
+import Foundation
---
+++ MagicChat/TextField.swift
@@ -0,0 +1,21 @@
+//
+//  TextField.swift
+//  MagicChat
+//
+//  Created by Асылбек Жилкайдаров on 29.05.17.
+//  Copyright © 2017 Test Inc. All rights reserved.
+//
+
+import UIKit
+
+class TextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
